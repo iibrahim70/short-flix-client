9791cab087db38d8e968d48af4dbae4cca6fa078
iibrahim70 <iibrahiim.dev@gmail.com>
2025-11-27 18:02:50 +0600

    :art: refactor Navbar component layout and update search placeholder text

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,29 +1,35 @@
-import { Film, Search } from "lucide-react";
+import { Search } from "lucide-react";
 import { Input } from "../ui/input";
 
 const Navbar = () => {
+  const navItems = ["Home", "Tv Shows", "Movies", "Recently Added", "My List"];
+
   return (
     <nav className="py-5 sticky left-0 top-0 z-10 bg-neutral-900 border-b">
       <div className="container flex items-center justify-between gap-5">
-        {/* left side */}
-        <div className="flex items-center gap-2.5">
-          <div className="size-10 rounded-lg bg-red-600 flex items-center justify-center">
-            <Film className="size-6" />
-          </div>
+        {/* Left side */}
+        <h5 className="text-red-500">Shortflix</h5>
 
-          <h5 className="tracking-tight">
-            Short<span className="text-red-600">flix</span>
-          </h5>
-        </div>
+        {/* Middle */}
+        <ul className="flex items-center justify-center gap-5 text-sm font-medium">
+          {navItems?.map((item) => (
+            <li
+              key={item}
+              className="cursor-pointer transition-all duration-300 hover:text-red-500 hover:underline underline-offset-10"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
 
-        {/* right side */}
+        {/* Right side */}
         <div className="flex items-center gap-2.5">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-400" />
 
             <Input
               type="text"
-              placeholder="Search videos..."
+              placeholder="Search videos or tags..."
               className="pl-10 w-80"
             />
           </div>
